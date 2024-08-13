e1209261af795253648fe67b6f4636a380bafd1d
Pavan Medi <pamedi@ibasis.net>
2024-08-13 19:17:45 +0530

    fav icon

--- admin_dashboard/header.php
+++ admin_dashboard/header.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Application</title>
-    <link href="/admin_dashboardhttps://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../assests/favicon.ico">
+    <title>OPSGuruu ADMIN Dashboard</title>
+    <link href="/https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
