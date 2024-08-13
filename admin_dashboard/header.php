642716205eac21ebe790c32a0a3a9d456d978182
Pavan Medi <pamedi@ibasis.net>
2024-08-13 19:18:51 +0530

    test

--- admin_dashboard/header.php
+++ admin_dashboard/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../assests/favicon.ico">
     <title>OPSGuruu ADMIN Dashboard</title>
-    <link href="/https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
